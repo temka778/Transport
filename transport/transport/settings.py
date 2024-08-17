fa67a112e84cafa5b423e40bfa94d00e52c9e44f
temka778 <113933105+temka778@users.noreply.github.com>
2024-08-17 13:36:12 +0400

    Удалил из settings.py константу STATICFILES_DIRS
    аааааааааааааааааааааааааааааааааа

--- transport/transport/settings.py
+++ transport/transport/settings.py
@@ -91,8 +91,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'services:index'
