354b01ecc2b5d1732a0d8832279c488695b3ce1a
Артём Курочкин <av1965k@yandex.ru>
2024-08-20 02:30:55 +0400

    Создал кастомный класс смены пароля, чтобы перенаправлял на нужный шаблон. Обновил урл.

--- transport/users/auth_urls.py
+++ transport/users/auth_urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import views
 from django.urls import path
 
-from .views import SignUp
+from .views import SignUp, PasswordChangeView
 
 app_name = 'auth'
 
@@ -10,7 +10,7 @@ urlpatterns = [
     path("login/", views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path("logout/", views.LogoutView.as_view(template_name='users/logged_out.html'), name="logout"),
     path("password_change/done/", views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name="password_change_done"),
-    path("password_change/", views.PasswordChangeView.as_view(template_name='users/password_change_form.html'), name="password_change"),
+    path("password_change/", PasswordChangeView.as_view(template_name='users/password_change_form.html'), name="password_change"),
     path("password_reset/done/", views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name="password_reset_done"),
     path("password_reset/", views.PasswordResetView.as_view(template_name='users/password_reset_form.html'), name="password_reset"),
     path("reset/<uidb64>/<token>/", views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name="password_reset_confirm"),
--- transport/users/views.py
+++ transport/users/views.py
@@ -2,6 +2,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordChangeView
 
 from .forms import CreationForm
 from .models import User
@@ -14,6 +15,13 @@ class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy('services:index')
     template_name = 'users/signup.html'
+
+
+class PasswordChangeView(PasswordChangeView):
+    success_url = reverse_lazy('auth:password_change_done')
+
+    def get_success_url(self):
+        return self.success_url
 
 
 class Profile(LoginRequiredMixin, DetailView):
