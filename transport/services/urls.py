b3093634e3a64e9663582d3715c5de4f5a721094
Артём Курочкин <av1965k@yandex.ru>
2024-09-05 02:34:17 +0400

    теперь сайт состоит из одной страницы

--- transport/services/urls.py
+++ transport/services/urls.py
@@ -3,8 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('services/', views.services, name='services'),
-    path('contacts/', views.contacts, name='contacts'),
-    #path('orders/', include('orders.urls', namespace='orders'))
+    path('', views.index, name='index')
 ]
--- transport/services/views.py
+++ transport/services/views.py
@@ -4,16 +4,6 @@ from . models import Technique
 
 def index(request):
     template = 'services/index.html'
-    return render(request, template)
-
-
-def services(request):
-    template = 'services/services.html'
     technique = Technique.objects.all()
     context = {'technique': technique}
     return render(request, template, context)
-
-
-def contacts(request):
-    template = 'services/contacts.html'
-    return render(request, template)
